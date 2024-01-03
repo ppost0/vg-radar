e1d0247369f8076b1d69477c5325222be253f293
ppost0 <p.post2321@yahoo.com>
2024-01-03 10:59:10 -0500

    Add catch-all route handler for unknown endpoints

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-
+// Serve main HTML
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+  return res.sendFile(path.join(__dirname, './index.html'));
 })
+
+
+// Catch-all route handler
+app.use('/*', (req, res) => {
+  res.status(404).send('Page not found - 404');
+})
+
+
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
